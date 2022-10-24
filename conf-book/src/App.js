e39d2a450470981947733e63492a9ef9eb7df4d9
VarunSH-15-11-2001 <varunhoskere@gmail.com>
2022-10-24 18:24:26 +0530

    version0
    trial with properties-cs490

--- conf-book/src/App.js
+++ conf-book/src/App.js
@@ -1,9 +1,16 @@
+import Component from "./Component1/comp";
+
+
 function App() {
+  console.log('This is where things begin - in the main app component');
   return (
     <div>
-      <h1>Hello live stupid update update again</h1>
+      <h1>Temporay header</h1>
+      <Component name="hello"></Component>
     </div>
   );
+
+  
 }
 
 export default App;
---
+++ conf-book/src/Component1/comp.js
@@ -0,0 +1,13 @@
+const Component = (props) => {
+    console.log('This is the second point of data flow - in the nested component.');
+
+    return (
+      <div>
+        <h1>{props.name}</h1>
+      </div>
+    );
+
+    
+  }
+  
+  export default Component;

9ce69828c807bfdb6c540877ba4ddbaacffdda5e
VarunSH-15-11-2001 <varunhoskere@gmail.com>
2022-10-24 18:58:24 +0530

    version1
    added floor changing meanscs490

--- conf-book/src/App.js
+++ conf-book/src/App.js
@@ -1,14 +1,14 @@
 import Component from "./Component1/comp";
 
 function App() {
-  console.log('This is where things begin - in the main app component');
+  // console.log('This is where things begin - in the main app component');
   return (
     <div>
       <h1>Temporay header</h1>
       <Component name="hello"></Component>
     </div>
   );
-  
+
 }
 
 export default App;
--- conf-book/src/Component1/comp.js
+++ conf-book/src/Component1/comp.js
@@ -1,8 +1,41 @@
+import "./ExpenseForm.css" ;
+
 const Component = (props) => {
-    console.log('This is the second point of data flow - in the nested component.');
+    // console.log('This is the second point of data flow - in the nested component.');
+    let ble;
+
+    const submitHandle = (event) => {
+      event.preventDefault();
+      ble=event.target.value;
+    };
+
     return (
       <div>
-        <h1>{props.name}</h1>
+        <form>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Floor </label>
+              <input
+                type="number"
+                min="1"
+                step="1"
+                value ={ble}
+              />
+            </div>
+            {/* <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                
+              />
+            </div> */}
+          </div>
+          <div className="new-expense__actions">
+            <button type="submit" onClick={submitHandle}>Display details</button>
+          </div>
+        </form>
       </div>
     ); 
   }
